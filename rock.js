c6948d83cd573b3fa38a984a3d1cc324fd8b38b4
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-06-29 16:24:49 -0400

    added game function

--- rock.js
+++ rock.js
@@ -1,3 +1,5 @@
+let playerWinner = 0
+let computerWinner = 0
 
 
 function getComputerChoice() {
@@ -9,25 +11,49 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
 
     if (/rock/i.test(playerSelection) == true && computerSelection == "Scissors"){
+        playerWinner = playerWinner + 1
         return "You Win! " + playerSelection + " beats " + computerSelection
     }
     if (/scissors/i.test(playerSelection) == true && computerSelection == "Paper"){
+        playerWinner = playerWinner + 1
         return "You Win! " + playerSelection + " beats " + computerSelection
     }
     if (/paper/i.test(playerSelection) == true && computerSelection == "Rock"){
+        playerWinner = playerWinner + 1
         return "You Win! " + playerSelection + " beats " + computerSelection
     }
     if (playerSelection == computerSelection){
         return "Tie " + playerSelection + " same as " + computerSelection
     }
     else { 
+        computerWinner = computerWinner + 1
         return "You Lose! " + computerSelection + " beats " + playerSelection
     }
 
   }
    
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice()
+function game() {
+
+    for (let i = 0; i < 5; i++){
+
+    let playerSelection = prompt() 
+console.log(playRound(playerSelection, getComputerChoice()))
+    }
+if (playerWinner > computerWinner) {
+    return "You Win!"
+}
+else return "You Lose!"
+
+
+}
+
+
+
+/*
+  const playerSelection = "Paper";
+  const computerSelection = getComputerChoice()*/
+  /*
   console.log(playRound(playerSelection, computerSelection));
   console.log(playerSelection)
   console.log(computerSelection)
+  */
