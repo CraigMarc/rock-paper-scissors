f5fc1cb6bc192832d38afa88c90be639b1bfd284
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-06-29 16:02:04 -0400

    added play round function

--- rock.js
+++ rock.js
@@ -6,4 +6,28 @@ function getComputerChoice() {
     return randomArray[randomNumber]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+
+    if (/rock/i.test(playerSelection) == true && computerSelection == "Scissors"){
+        return "You Win! " + playerSelection + " beats " + computerSelection
+    }
+    if (/scissors/i.test(playerSelection) == true && computerSelection == "Paper"){
+        return "You Win! " + playerSelection + " beats " + computerSelection
+    }
+    if (/paper/i.test(playerSelection) == true && computerSelection == "Rock"){
+        return "You Win! " + playerSelection + " beats " + computerSelection
+    }
+    if (playerSelection == computerSelection){
+        return "Tie " + playerSelection + " same as " + computerSelection
+    }
+    else { 
+        return "You Lose! " + computerSelection + " beats " + playerSelection
+    }
+
+  }
+   
+  const playerSelection = "Rock";
+  const computerSelection = getComputerChoice()
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playerSelection)
+  console.log(computerSelection)
